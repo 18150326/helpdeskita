a44c263e74f7edda29e908d52006666a442d1cf1
18150326 <18150326@aguascalientes.tecnm.mx>
2022-01-31 23:59:24 -0600

    modal firmar reporte (usuario)

---
+++ vistas/Reportes/modalFirmar.php
@@ -0,0 +1,32 @@
+<form id="frmFirmar" method="POST">
+
+<!-- Modal Firmar -->
+<div class="modal fade" id="modalFirmar" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Aviso de confirmación</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <!-- Formulario en modal -->
+        <div class="row">
+            <div class="col-sm-12">
+                <label for="men"> Al hacer click en aceptar, usted confirma que el reporte está firmado por la persona solicitada y 
+                  se le avisara al personal del centro de cómputo para recoger el documento, ¿esta seguro/a de esto?
+                </label>
+            </div>
+        </div>
+      </div>
+
+      <div class="modal-footer">
+        <span class="btn btn-secondary" data-dismiss="modal">Cerrar</span>
+        <button class="btn btn-primary" >Aceptar</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+</form>
